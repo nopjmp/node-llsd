bf5e27987384036c22c5958cd22b1c222830a897
Kyle Thompson <noopjump@gmail.com>
2015-01-01 00:07:31 -0600

    :fireworks: tests and rearranged callbacks to fit node convention

--- index.js
+++ index.js
@@ -5,143 +5,143 @@ var array = require('./types/array.js')
 var map = require('./types/map.js')
 var convert = require('./types/convert.js')
 
-var utils = require('./utils.js')
+var helpers = require('./lib/helpers.js')
 
 //undef is an array to give is some value, might want to make it null.
 var generateSubParser = function(name, attrs, subparser, state) {
-	switch (name) {
-		case 'llsd':
-			return new llsdp()
-			break
-		case 'undef':
-		case 'array':
-			return new array(subparser)
-			break
-		case 'map':
-			return new map(subparser)
-			break
-		case 'key':
-			state.in_data = true
-			return new subparser.key()
-			break
-		case 'integer':
-			state.in_data = true
-			return new convert(0, subparser, utils.stringToInteger)
-			break
-		case 'binary':
-			state.in_data = true
-			return new convert("", subparser, utils.convertBase64.bind(undefined,
-				attrs.encoding))
-		case 'uri':
-		case 'string':
-			state.in_data = true
-			return new convert("", subparser, utils.noop)
-			break
-		case 'real':
-			state.in_data = true
-			return new convert(0.0, subparser, Float.parseFloat)
-			break
-		case 'boolean':
-			state.in_data = true
-			return new convert(false, subparser, utils.stringToBoolean)
-			break
-		case 'uuid':
-			//TODO: use node-uuid to check validity
-			state.in_data = true
-			return new convert("00000000-0000-0000-0000-000000000000", subparser,
-				utils.noop)
-			break
-		case 'date':
-			state.in_data = true
-			return new convert(new Date(), subparser, utils.stringToDate)
-			break
-		default:
-			throw new Error("unimplemented: " + name)
-	}
+  switch (name) {
+    case 'llsd':
+      return new llsdp()
+      break
+    case 'undef':
+    case 'array':
+      return new array(subparser)
+      break
+    case 'map':
+      return new map(subparser)
+      break
+    case 'key':
+      state.in_data = true
+      return new subparser.key()
+      break
+    case 'integer':
+      state.in_data = true
+      return new convert(0, subparser, helpers.stringToInteger)
+      break
+    case 'binary':
+      state.in_data = true
+      return new convert("", subparser, helpers.convertBase64.bind(undefined,
+        attrs.encoding))
+    case 'uri':
+    case 'string':
+      state.in_data = true
+      return new convert("", subparser, helpers.noop)
+      break
+    case 'real':
+      state.in_data = true
+      return new convert(0.0, subparser, Float.parseFloat)
+      break
+    case 'boolean':
+      state.in_data = true
+      return new convert(false, subparser, helpers.stringToBoolean)
+      break
+    case 'uuid':
+      //TODO: use node-uuid to check validity
+      state.in_data = true
+      return new convert("00000000-0000-0000-0000-000000000000", subparser,
+        helpers.noop)
+      break
+    case 'date':
+      state.in_data = true
+      return new convert(new Date(), subparser, helpers.stringToDate)
+      break
+    default:
+      throw new Error("unimplemented: " + name)
+  }
 }
 
-exports.Parser = function(error, callback) {
-	var self = this
-	this.state = {
-		working: false,
-		in_data: false,
-		tag_stack: []
-	}
-	this.subparser = null // current subparser
+exports.Parser = function(callback, error) {
+  var self = this
+  this.state = {
+    working: false,
+    in_data: false,
+    tag_stack: []
+  }
+  this.subparser = null // current subparser
 
-	xml_parser = new expat.Parser('UTF-8')
-	xml_parser.on('startElement', function(name, attrs) {
-		if (self.state.in_data) {
-			throw new Error("array tag inside data tag")
-			xml_parser.stop()
-		}
-		self.state.tag_stack.push(name)
-		if (name === 'llsd') {
-			if (self.state.working) {
-				throw new Error("cannot parse another llsd")
-				xml_parser.stop()
-			} else {
-				self.state.working = true
-			}
-		}
-		//generates a recursive call tree to translate llsd to json
-		try {
-			self.subparser = generateSubParser(name, attrs, self.subparser,
-				self.state)
-		} catch (e) {
-			error(e)
-			xml_parser.stop()
-		}
-	})
+  xml_parser = new expat.Parser('UTF-8')
+  xml_parser.on('startElement', function(name, attrs) {
+    if (self.state.in_data) {
+      throw new Error("array tag inside data tag")
+      xml_parser.stop()
+    }
+    self.state.tag_stack.push(name)
+    if (name === 'llsd') {
+      if (self.state.working) {
+        throw new Error("cannot parse another llsd")
+        xml_parser.stop()
+      } else {
+        self.state.working = true
+      }
+    }
+    //generates a recursive call tree to translate llsd to json
+    try {
+      self.subparser = generateSubParser(name, attrs, self.subparser,
+        self.state)
+    } catch (e) {
+      error(e)
+      xml_parser.stop()
+    }
+  })
 
-	xml_parser.on('endElement', function(name) {
-		var last_name = self.state.tag_stack.pop()
+  xml_parser.on('endElement', function(name) {
+    var last_name = self.state.tag_stack.pop()
 
-		if (last_name !== name) {
-			throw new Error("unmatched tag: " + name + "!=" + last_name)
-			xml_parser.stop()
-		}
+    if (last_name !== name) {
+      throw new Error("unmatched tag: " + name + "!=" + last_name)
+      xml_parser.stop()
+    }
 
-		if (name === "llsd") {
-			self.state.working = false
-			xml_parser.stop()
+    if (name === "llsd") {
+      self.state.working = false
+      xml_parser.stop()
 
-			//return the data after the llsd tag (array or map)
-			callback(self.subparser.data.data)
-		}
-		self.subparser = self.subparser.end()
-			//always reset this to false in endElement to reset the check
-		self.state.in_data = false
-	})
+      //return the data after the llsd tag (array or map)
+      callback(self.subparser.data.data)
+    }
+    self.subparser = self.subparser.end()
+      //always reset this to false in endElement to reset the check
+    self.state.in_data = false
+  })
 
-	xml_parser.on('text', function(text) {
-		if (self.state.in_data == true) {
-			self.subparser.newData(text)
-		} else {
-			throw new Error("got data in non-data section")
-			xml_parser.stop()
-		}
-	})
+  xml_parser.on('text', function(text) {
+    if (self.state.in_data == true) {
+      self.subparser.newData(text)
+    } else {
+      throw new Error("got data in non-data section")
+      xml_parser.stop()
+    }
+  })
 
-	xml_parser.on('error', error)
+  xml_parser.on('error', error)
 
-	xml_parser.on('end', function() {
-		if (self.state.working) {
-			error("end before finding </llsd>")
-		}
-	})
+  xml_parser.on('end', function() {
+    if (self.state.working) {
+      error("end before finding </llsd>")
+    }
+  })
 
-	this._called_parse = false
-	this.end = function(data) {
-		if (self._called_parse) {
-			error("called parse twice");
-		} else {
-			xml_parser.end(data)
-		}
-	}
+  this._called_parse = false
+  this.end = function(data) {
+    if (self._called_parse) {
+      error("called parse twice");
+    } else {
+      xml_parser.end(data)
+    }
+  }
 }
 
-exports.parse = function(data, error, callback) {
-	var p = new exports.Parser(error, callback);
-	p.end(data);
+exports.parse = function(data, callback, error) {
+  var p = new exports.Parser(callback, error);
+  p.end(data);
 }
--- tests/llsd_undef.js
+++ tests/llsd_undef.js
@@ -1,2 +1,13 @@
-var llsd_parser = require('../index.js');
-llsd_parser.parse("<llsd><undef /></llsd>", console.error, console.log)
+var tape = require('tape')
+
+var llsd = require('../index.js');
+
+tape('undef test', function(t) {
+	t.plan(2)
+	llsd.parse("<llsd><undef /></llsd>", function(data) {
+		t.equal(typeof data, 'object', "object")
+		t.equal(data.length, 0, "array length")
+	}, function(e) {
+		t.fail(e)
+	})
+})
